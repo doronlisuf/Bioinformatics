d4f86902e186e0e455910705b1c7f8903e33b2e4
r-clarke <62532387+r-clarke@users.noreply.github.com>
2021-09-21 11:36:23 -0400

    Working on density plot

--- Ricky.R
+++ Ricky.R
@@ -1,17 +1,19 @@
 library(matrixStats)
+library(ggplot2)
 
 df <- read.table("GSE119290_Readhead_2018_RNAseq_gene_counts.txt")
 
 matrixDF <- matrix(as.numeric(unlist(df)),nrow=nrow(df))
 matrixDF
-x <- rowRanges(matrixDF, rows = NULL, cols = NULL, na.rm = FALSE, dim. = dim(matrixDF), useNames = NA)
+r <- rowRanges(matrixDF, rows = NULL, cols = NULL, na.rm = FALSE, dim. = dim(matrixDF), useNames = NA)
 
-x
-
-hist(matrixDF[1,])
-
-
-for(i in 1:26000) {
-  hist(matrixDF[i,])
+ranges <- 0
+for(i in 1:26264) {
+  ranges[i] <- r[i,2]-r[i,1]
 }
+df_range <- as.data.frame(ranges)
 
+
+p <- ggplot(df_range, aes(x=ranges)) + geom_density()
+
+p
