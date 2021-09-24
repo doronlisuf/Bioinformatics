6b2b9f1942fadcffc48025a0fe09b82ff6902cda
Richard Clarke <ricky.jiangclarke@gmail.com>
2021-09-24 16:19:30 -0400

    Density Plot Added

--- Ricky.R
+++ Ricky.R
@@ -1,17 +1,16 @@
 library(matrixStats)
 library(ggplot2)
+library(DESeq2)
 
 df <- read.table("GSE119290_Readhead_2018_RNAseq_gene_counts.txt")
 
-matrixDF <- matrix(as.numeric(unlist(df)),nrow=nrow(df))
-matrixDF
-r <- rowRanges(matrixDF, rows = NULL, cols = NULL, na.rm = FALSE, dim. = dim(matrixDF), useNames = NA)
+matrix_of_data <- matrix(as.numeric(unlist(df)),nrow=nrow(df))
 
-ranges <- 0
+matrix_of_ranges <- rowRanges(matrix_of_data, rows = NULL, cols = NULL, na.rm = FALSE, dim. = dim(matrix_of_data), useNames = NA)
+
+vector_of_ranges <- 0
 for(i in 1:26364) {
-  ranges[i] <- r[i,2]-r[i,1]
+  vector_of_ranges[i] <- matrix_of_ranges[i,2]-matrix_of_ranges[i,1]
 }
-hist(ranges)
-max(ranges)
-plot(density(ranges))
 
+plot(density(vector_of_ranges), log='x')
